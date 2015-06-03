5cb835c20ab460d0f2a207b5ec8006eb5bed87c5
Amy Boyd <amy@amyboyd.co.uk>
2015-06-03 19:05:25 +0100

    Add coding test.

---
+++ your-code.js
@@ -0,0 +1,11 @@
+angular.module('jsCodingTest', [
+    // Add module dependencies here.
+]);
+
+angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function() {
+    // Your JavaScript goes here.
+    // Your code should use our JS library's `PackagesFactory.searchPackages` method to search
+    // for burritos that can be delivered to the Houses of Parliament in London.
+    // The API URL that should be called is:
+    // https://api.citypantry.com/packages/search?name=Burrito&postcode=SW1A%200AA
+});
